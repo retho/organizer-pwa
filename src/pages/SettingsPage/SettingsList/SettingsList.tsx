2009e546378ca808ca8d9fa9146b1206e066009e
Safonov Evgeny <safonov.e.s@gmail.com>
2022-10-14 18:45:14 +0300

    []

--- src/pages/SettingsPage/SettingsList/SettingsList.tsx
+++ src/pages/SettingsPage/SettingsList/SettingsList.tsx
@@ -1,12 +1,34 @@
 import './styles.scss';
 
+import InfoIcon from '@mui/icons-material/Info';
+import {List, ListItem, ListItemButton, ListItemIcon, ListItemText, Paper} from '@mui/material';
 import React, {FC, useEffect} from 'react';
 import {bem} from 'src/core/bem';
 import {useDispatch} from 'src/core/redux';
+import {Href, stringifyRoute} from 'src/core/router';
+import {routes} from 'src/router';
 import {loadTodoList} from 'src/store/slices/todoList';
 
+type SettingsListItemProps = {
+  href: Href;
+  icon: JSX.Element;
+  text: string;
+};
+const SettingsListItem: FC<SettingsListItemProps> = ({href, icon, text}) => {
+  return (
+    <ListItem disablePadding>
+      <Paper style={{width: '100%'}}>
+        <ListItemButton component="a" href={href}>
+          <ListItemIcon>{icon}</ListItemIcon>
+          <ListItemText primary={text} />
+        </ListItemButton>
+      </Paper>
+    </ListItem>
+  );
+};
+
 const root = bem(module.id, 'SettingsList');
-const TodoList: FC = () => {
+const SettingsList: FC = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -15,9 +37,15 @@ const TodoList: FC = () => {
 
   return (
     <div className={root()}>
-      <div>About</div>
+      <List>
+        <SettingsListItem
+          href={stringifyRoute(routes.settings, {}, {})}
+          icon={<InfoIcon />}
+          text="About"
+        />
+      </List>
     </div>
   );
 };
 
-export default TodoList;
+export default SettingsList;
