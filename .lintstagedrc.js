74a62c28410df9dae3d93bdc1449a40582a0acf0
Safonov Evgeny <safonov.e.s@gmail.com>
2022-10-14 13:10:04 +0300

    []

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,5 +1,5 @@
 // https://github.com/okonet/lint-staged
 module.exports = {
   'src/**/*.{ts,tsx}': ['eslint --config .eslintrc.prettier.js --fix', 'prettier --write', 'eslint --max-warnings=0'],
-  'src/**/*': () => 'npm run test:ci',
+  // 'src/**/*': () => 'npm run test:ci',
 }
